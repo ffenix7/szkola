87e1a0d97fee0a7e1b1029fe50a08b450ae0e173
Filip Gębala <filip.gebala@interia.pl>
2025-09-10 12:42:46 +0200

    blitek webowe #2

---
+++ Szkola/Aplikacje webowe/blitek/02 - templates/app.py
@@ -0,0 +1,23 @@
+import flask 
+
+app = flask.Flask(__name__)
+
+users = [
+    {'id': 1, 'name': 'Alice', 'email': 'alice@example.com'},
+    {'id': 2, 'name': 'Bob', 'email': 'bob@example.com'},
+    {'id': 3, 'name': 'Charlie', 'email': 'charlie@example.com'}
+]
+
+@app.route(rule='/')
+def index():
+    return flask.render_template(template_name_or_list='index.html', title='Home', users=users)
+
+@app.route(rule='/user/<int:id>')
+def user(id):
+    for user in users:
+        if user['id'] == id:
+            user = next((user for user in users if user['id'] == id), None)
+            return flask.render_template(template_name_or_list='user.html', user=user, title="Profil użytkownika")
+
+if __name__ == '__main__':
+    app.run(debug=True)
