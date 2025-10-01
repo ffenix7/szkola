e40555b5d2e62a81534e2b2d5a845027c3e29b5d
Filip Gębala <filip.gebala@interia.pl>
2025-10-01 12:42:46 +0200

    fixed express

--- Szkola/Aplikacje webowe/niemiec/lekcja_js_3/test.js
+++ Szkola/Aplikacje webowe/niemiec/lekcja_js_3/test.js
@@ -17,24 +17,24 @@ function luckyMiddleware(req, res, next) {
 }
 
 app.use(loggerMiddleware);
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'style.css'));
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/podstrona.html', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/podstrona.html'));
+  res.sendFile(path.join(__dirname, 'podstrona.html'));
 });
 
 app.get('/podstrona2.html', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/podstrona2.html'));
+  res.sendFile(path.join(__dirname, 'podstrona2.html'));
 });
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/style.css'));
-});
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
