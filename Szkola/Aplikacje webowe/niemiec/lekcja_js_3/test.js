6f2741d4214f64e7e174a0df46365a7e7a345b12
Filip Gębala <filip.gebala@interia.pl>
2026-02-18 09:05:51 +0100

    heap (not working)

--- Szkola/Aplikacje webowe/niemiec/lekcja_js_3/test.js
+++ Szkola/Aplikacje webowe/niemiec/lekcja_js_3/test.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const path = require('node:path')
-const app = express()
-const port = 44444
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = 44444;
 
 function loggerMiddleware(req, res, next) {
   console.log(`Adres żądania: ${req.path}, Data: ${new Date()}`);
@@ -16,20 +20,25 @@ function luckyMiddleware(req, res, next) {
   }
 }
 
+
 app.use(loggerMiddleware);
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 
 app.get('/style.css', (req, res) => {
   res.sendFile(path.join(__dirname, 'style.css'));
 });
 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+
 app.get('/podstrona.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'podstrona.html'));
 });
+
 
 app.get('/podstrona2.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'podstrona2.html'));
